056ad4323702e9376b6de0a59d730fdf71d7da8b
t24020961 <esteban.toro@etu.univ-amu.fr>
2025-12-31 00:21:43 +0100

    Adding the very first item, I have stopped the video at 13:28

--- src/main/java/sendofire/testmod/TestMod.java
+++ src/main/java/sendofire/testmod/TestMod.java
@@ -4,14 +4,15 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sendofire.testmod.item.ModItems;
 
 public class TestMod implements ModInitializer {
-
 	public static final String MOD_ID = "testmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 
 	}
 }
---
+++ src/main/java/sendofire/testmod/item/ModItems.java
@@ -0,0 +1,26 @@
+package sendofire.testmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import sendofire.testmod.TestMod;
+
+public class ModItems {
+    public static final Item  KINGLY_SOUL_EYE = registerItem("kingly_soul_eye", new Item(new Item.Settings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TestMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TestMod.LOGGER.info("Registering Mod Items for " + TestMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(KINGLY_SOUL_EYE);
+        });
+    }
+}
